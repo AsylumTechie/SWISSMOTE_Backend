7556badc45fdfcc282c0921ab631799513e26eee
100X_Dev <admin@Admins-MacBook-Pro.local>
2025-02-07 15:03:53 +0530

    Deploy

--- index.js
+++ index.js
@@ -1,47 +1,49 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const socketIO = require('socket.io');
-const http = require('http');
-const auth = require('./login/loginAuth');
-const signupAuth = require('./signup/signupAuth');
-const eventRoutes = require('./routes/eventRoutes');
-const { connectDB } = require('./config/database');
-const socketHandler = require('./socket');  // If needed
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const socketIO = require("socket.io");
+const http = require("http");
+const auth = require("./login/loginAuth");
+const signupAuth = require("./signup/signupAuth");
+const eventRoutes = require("./routes/eventRoutes");
+const { connectDB } = require("./config/database");
+const socketHandler = require("./socket"); // If needed
 
 const app = express();
 const server = http.createServer(app);
 
 // CORS configuration for HTTP requests
-app.use(cors({
-  origin: 'http://192.168.1.103:3000',  // Allow your frontend to access the server
-  methods: ['GET', 'POST'],
-}));
+app.use(
+  cors({
+    origin: ["http://192.168.1.102:3000", "https://swissmotefrontend-git-main-rohit-chaudharys-projects-a08c6dbc.vercel.app"], // Allow your frontend to access the server
+    methods: ["GET", "POST"],
+  })
+);
 
 // Socket.io initialization with CORS configuration
 const io = socketIO(server, {
   cors: {
-    origin: 'http://192.168.1.103:3000',  // Allow your frontend to access WebSocket
-    methods: ['GET', 'POST'],            // Allow necessary methods
-    allowedHeaders: ['my-custom-header'], // Optional if you need custom headers
-    credentials: true                    // Allow cookies if needed
-  }
+    origin: ["http://localhost:3000", "http://192.168.1.102:3000"], // Allow your frontend to access WebSocket
+    methods: ["GET", "POST"], // Allow necessary methods
+    allowedHeaders: ["my-custom-header"], // Optional if you need custom headers
+    credentials: true, // Allow cookies if needed
+  },
 });
 
 app.use(express.json());
 
 // Database connection
-connectDB(); 
+connectDB();
 
 // Routes
-app.use('/api', auth);
-app.use('/api', signupAuth);
-app.use('/api', eventRoutes);
+app.use("/api", auth);
+app.use("/api", signupAuth);
+app.use("/api", eventRoutes);
 
 // Socket.IO for real-time updates
-io.on('connection', socketHandler(io)); // Assuming socketHandler manages connections
+io.on("connection", socketHandler(io)); // Assuming socketHandler manages connections
 
 // Start the server
 server.listen(8080, () => {
-  console.log('Server running on http://localhost:8080');
+  console.log("Server running on http://localhost:8080");
 });
